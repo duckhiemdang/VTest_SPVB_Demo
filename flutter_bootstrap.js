b754efe77fc3b320a5c599197f6ce486917e0abd
duckhiem2003 <duckhiem2003@gmail.com>
2025-05-20 15:03:44 +0700

    update to remove masan and mdlz stuff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1905679227"
+    serviceWorkerVersion: "2776192689"
   }
 });
